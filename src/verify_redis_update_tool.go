a184d8ba12a6031c7ba2d5796895823b5a128d76
huchangwei <huchangwei@360.cn>
2016-09-13 18:09:51 +0800

    读出redis的所有数据，排序好写到txt文件中

--- src/verify_redis_update_tool.go
+++ src/verify_redis_update_tool.go
@@ -19,8 +19,7 @@ func getCurrentPath() string {
 	}
 	return strings.Replace(dir, "\\", "/", -1)
 }
-func callredis_updateEXE(filePath string, argA, argB string) {
-
+func callredis_updateEXE(filePath string, filename string, argA, argB string) {
 	//往redis里插入数据之前先清空一下redis
 	client := connRedis()
 	defer client.Close()
@@ -30,9 +29,9 @@ func callredis_updateEXE(filePath string, argA, argB string) {
 		panic(err.Error())
 	}
 
-	//	fmt.Println("Current path:", getCurrentPath())
-	cmd := exec.Command(filePath, argA, argB)
-
+	fmt.Println("Current path:", getCurrentPath())
+	cmd := exec.Command(filePath+filename, argA, argB)
+	cmd.Dir = filePath
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err = cmd.Run()
@@ -40,46 +39,135 @@ func callredis_updateEXE(filePath string, argA, argB string) {
 		fmt.Println("Run error:", err.Error())
 		panic(err.Error())
 	}
-
 	fmt.Println("Output info:", out.String())
+}
+func dataConverStrings(res interface{}, err error) []string {
+	if err != nil {
+		panic(err.Error())
+	}
+	value, err := redis.Strings(res, err)
+	if err != nil {
+		panic(err.Error())
+	}
+	return value
+}
+
+func dataConverString(res interface{}, err error) string {
+	if err != nil {
+		panic(err.Error())
+	}
+	value, err := redis.String(res, err)
+	if err != nil {
+		panic(err.Error())
+	}
+	return value
+}
+func redisDoStrings(client redis.Conn, args ...string) []string {
+	switch len(args) {
+	case 1:
+		res, err := client.Do(args[0])
+		value := dataConverStrings(res, err)
+		return value
+	case 2:
+		res, err := client.Do(args[0], args[1])
+		value := dataConverStrings(res, err)
+		return value
+	case 3:
+		res, err := client.Do(args[0], args[1], args[2])
+		value := dataConverStrings(res, err)
+		return value
+	default:
+		fmt.Println("not args into")
+		panic("unrecognized escape character")
+	}
 
 }
 
-func operateRedis() {
+func redisDoString(client redis.Conn, args ...string) string {
+	switch len(args) {
+	case 1:
+		res, err := client.Do(args[0])
+		value := dataConverString(res, err)
+		return value
+	case 2:
+		res, err := client.Do(args[0], args[1])
+		value := dataConverString(res, err)
+		return value
+	case 3:
+		res, err := client.Do(args[0], args[1], args[2])
+		value := dataConverString(res, err)
+		return value
+	default:
+		fmt.Println("not args into")
+		panic("unrecognized escape character")
+	}
+
+}
+func operateRedis(fileName string) {
+	file, ERR := os.Create(fileName)
+	if ERR != nil {
+		panic(ERR.Error())
+	}
+	defer file.Close()
+
 	client := connRedis()
 	defer client.Close()
 
-	res, err := client.Do("KEYS", "*")
-	if err != nil {
-		fmt.Println("operate redis error:", err.Error())
-		panic(err.Error())
-	}
-
-	keys, err := redis.Strings(res, err)
-	if err != nil {
-		panic(err.Error())
-	}
-
+	keys := redisDoStrings(client, "KEYS", "*")
 	for i, k := range keys {
 		fmt.Println("keys:", i, k)
 		_, err := client.Do("SADD", "redisOld", k)
 		if err != nil {
 			panic(err.Error())
 		}
+
+	}
+	redisOld := redisDoStrings(client, "SORT", "redisOld", "ALPHA")
+	for _, k := range redisOld {
+		//fmt.Println("new Keys:", i, k)
+		keytype := redisDoString(client, "type", k)
+
+		if keytype == "string" {
+			value := redisDoString(client, "GET", k)
+			FileWrite(file, "\n")
+			FileWrite(file, k+":"+value)
+		} else if keytype == "hash" {
+			value := redisDoStrings(client, "HKEYS", k)
+			hashkey := SortHashKey(value, client)
+			fmt.Println(hashkey)
+			WriteHashValuetoFile(k, hashkey, file, client)
+		}
 	}
 
-	res, err = client.Do("SORT", "redisOld", "ALPHA")
+}
+func FileWrite(file *os.File, str string) {
+	_, err := file.WriteString(str)
 	if err != nil {
 		panic(err.Error())
 	}
-	redisOld, err := redis.Strings(res, err)
+}
+func SortHashKey(key []string, client redis.Conn) []string {
+	for _, k := range key {
+		_, err := client.Do("SADD", "HashKey", k)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	value := redisDoStrings(client, "SORT", "HashKey", "ALPHA")
+	return value
+}
+func WriteHashValuetoFile(hash string, keys []string, file *os.File, client redis.Conn) {
+	FileWrite(file, "\n")
+	FileWrite(file, hash+":\n")
+	for _, k := range keys {
+		value := redisDoString(client, "HGET", hash, k)
+		FileWrite(file, k+"    ")
+		FileWrite(file, value+"\n")
+	}
+	_, err := client.Do("DEL", "HashKey")
 	if err != nil {
 		panic(err.Error())
 	}
-	for i, k := range redisOld {
-		fmt.Println("new Keys:", i, k)
-	}
-
 }
 func connRedis() redis.Conn {
 	client, err := redis.Dial("tcp", "localhost:6379")
@@ -92,6 +180,11 @@ func connRedis() redis.Conn {
 }
 func main() {
 
-	callredis_updateEXE("./new/redis_update.exe", "-log", "true")
-	operateRedis()
+	filePath := getCurrentPath() + "/old/"
+	callredis_updateEXE(filePath, "redis_update.exe", "-log", "true")
+	operateRedis("OldData.txt")
+
+	filePath = getCurrentPath() + "/new/"
+	callredis_updateEXE(filePath, "/redis_update.exe", "-log", "true")
+	operateRedis("NewData.txt")
 }
